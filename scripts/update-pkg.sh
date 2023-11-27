20697bfad16fb8fae489d3741f32d5a3eff9627a
up-tri <ktarou78@gmail.com>
2023-11-27 20:17:55 +0900

    update pkgs

--- scripts/update-pkg.sh
+++ scripts/update-pkg.sh
@@ -1,9 +1,10 @@
 git checkout develop && git pull
 
-branch_name="feature/update_pkgs_$(date +%Y%m%d)"
+branch_name="feature/update_pkgs_$(date +%Y%m%d%H)"
 
 git checkout -b $branch_name
 npx npm-check-updates -u
+rm -rf package-lock.json yarn.lock
 npm install
 
 npm run test && git add . && git commit -m "update pkgs" && git push -u origin $branch_name && gh pr create --fill
